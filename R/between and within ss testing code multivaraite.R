8d03340a599d82ebe744e841e7b802a632f1e2b3
Greg McMahan <gmcmacran@gmail.com>
2019-10-27 15:31:27 -0500

    multivariate between within first push.

--- R/between and within ss testing code multivaraite.R
+++ R/between and within ss testing code multivaraite.R
@@ -9,6 +9,7 @@ cols <- 1
 x0 <- matrix(0, nrow = rows, ncol = cols)
 
 x0[,1] <- c(11, 17, 16, 14, 15, 12, 10, 15, 19, 11, 23, 20, 18, 17, 27, 33, 22, 26, 28)
+#x0[,2] <- c(11, 17, 16, 14, 15, 12, 10, 15, 19, 11, 23, 20, 18, 17, 27, 33, 22, 26, 28)
 
 y0 <- matrix(c(rep(1, 5), rep(2, 5), rep(3, 4), rep(4, 5)), nrow = rows, ncol = 1)
 
@@ -23,19 +24,54 @@ for(i in seq_along(1:length(unique(y0))) ){
 groupMeans
 grandMean
 
+# Weighted between sum of squares
+betweenSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
 pis <- matrix(c(5, 5, 4, 5), nrow = length(unique(y0)), ncol = 1)
 
+for(i in seq_along(1:nrow(betweenSS))) {
+  for(j in seq_along(1:ncol(betweenSS))) {
+    temp <- matrix(-1, nrow = nrow(groupMeans), ncol = 1)
+    for(k in seq_along(1:nrow(temp))) {
+      tempM1 <- groupMeans[k, ] - grandMean
+      tempM2 <- groupMeans[k, ] - grandMean
 
-betweenSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
-temp <- matrix(-1, nrow = length(unique(y0)), ncol =  ncol(grandMean))
-for(i in seq_along(1:nrow(temp))){
-  temp[i, ] <- pis[i, 1]*(groupMeans[i, ] - grandMean) * (groupMeans[i, ] - grandMean)
+      tempM1 <- matrix(tempM1, nrow = 1, ncol = length(tempM1))
+      tempM2 <- matrix(tempM2, nrow = 1, ncol = length(tempM2))
+      tempM2 <- t(tempM2)
+
+      temp[k, 1] <- pis[k]*tempM1 %*% tempM2
+    }
+    betweenSS[i, j] <- colSums(temp)
+  }
 }
-temp
-
-betweenSS[1, 1] <- colSums(temp)
 betweenSS
-rm(temp)
+
+# Weighted within sum of squares
+withinSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
+
+for(i in seq_along(1:nrow(withinSS))) {
+  for(j in seq_along(1:ncol(withinSS))) {
+    temp <- matrix(-1, nrow = nrow(x0), ncol = 1)
+    for(k in seq_along(1:nrow(temp))) {
+
+      currentGroupMean <- groupMeans[which(y0[k,] == unique(y0))]
+
+      tempM1 <- x0[k, ] - currentGroupMean
+      tempM2 <- x0[k, ] - currentGroupMean
+
+      tempM1 <- matrix(tempM1, nrow = 1, ncol = length(tempM1))
+      tempM2 <- matrix(tempM2, nrow = 1, ncol = length(tempM2))
+      tempM2 <- t(tempM2)
+
+
+      temp[k, 1] <- tempM1 %*% tempM2
+    }
+    withinSS[i, j] <- colSums(temp)
+  }
+}
+withinSS
+
+
 
 # Weighted within sum of squares
 withinSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
