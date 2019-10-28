2d47ff75341fa30168a3c5fb07c075f901782286
Greg McMahan <gmcmacran@gmail.com>
2019-10-27 19:59:04 -0500

    Updating multivarite testing

--- R/between and within ss testing code multivaraite.R
+++ R/between and within ss testing code multivaraite.R
@@ -3,37 +3,48 @@
 # SSE in book is W in dann
 # SSTO in book is Sigma
 
-rows <- 19
-cols <- 1
+compiler::enableJIT(3)
+rows <- 1000
+cols <- 100
 
 x0 <- matrix(0, nrow = rows, ncol = cols)
 
-x0[,1] <- c(11, 17, 16, 14, 15, 12, 10, 15, 19, 11, 23, 20, 18, 17, 27, 33, 22, 26, 28)
-#x0[,2] <- c(11, 17, 16, 14, 15, 12, 10, 15, 19, 11, 23, 20, 18, 17, 27, 33, 22, 26, 28)
+# x0[,1] <- c(11, 17, 16, 14, 15, 12, 10, 15, 19, 11, 23, 20, 18, 17, 27, 33, 22, 26, 28)
+# #x0[,2] <- c(11, 17, 16, 14, 15, 12, 10, 15, 19, 11, 23, 20, 18, 17, 27, 33, 22, 26, 28)
+# x0[,2] <- 1:rows
 
-y0 <- matrix(c(rep(1, 5), rep(2, 5), rep(3, 4), rep(4, 5)), nrow = rows, ncol = 1)
+# y0 <- matrix(c(rep(1, 5), rep(2, 5), rep(3, 4), rep(4, 5)), nrow = rows, ncol = 1)
 
+for(j in seq_along(1:ncol(x0))){
+  x0[,j] <- rnorm(rows, 0, 10)
+}
+
+y0 <- matrix(c(rep(0, floor(rows/2)), rep(1, ceiling(rows/2))), nrow = rows, ncol = 1)
+mean(y0)
 
 # Group wise means
 grandMean <- matrix(colMeans(x0), nrow = 1, ncol = ncol(x0))
 groupMeans <- matrix(0, nrow = length(unique(y0)), ncol = ncol(x0))
 
-for(i in seq_along(1:length(unique(y0))) ){
-  groupMeans[i, ] <- mean(x0[which(y0 == unique(y0)[i, ]), ], )
+for(i in seq_along(1:nrow(groupMeans)) ){
+  for(j in seq_along(1:ncol(groupMeans))){
+    groupMeans[i, j] <- mean(x0[which(y0 == unique(y0)[i, 1]), j] )
+  }
 }
-groupMeans
-grandMean
+#groupMeans
+#grandMean
 
 # Weighted between sum of squares
 betweenSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
-pis <- matrix(c(5, 5, 4, 5), nrow = length(unique(y0)), ncol = 1)
+#pis <- matrix(c(5, 5, 4, 5), nrow = length(unique(y0)), ncol = 1)
+pis <- matrix(c(floor(rows/2), ceiling(rows/2)), nrow = length(unique(y0)), ncol = 1)
 
 for(i in seq_along(1:nrow(betweenSS))) {
   for(j in seq_along(1:ncol(betweenSS))) {
     temp <- matrix(-1, nrow = nrow(groupMeans), ncol = 1)
     for(k in seq_along(1:nrow(temp))) {
-      tempM1 <- groupMeans[k, ] - grandMean
-      tempM2 <- groupMeans[k, ] - grandMean
+      tempM1 <- groupMeans[k, i] - grandMean[1,i]
+      tempM2 <- groupMeans[k, j] - grandMean[1,j]
 
       tempM1 <- matrix(tempM1, nrow = 1, ncol = length(tempM1))
       tempM2 <- matrix(tempM2, nrow = 1, ncol = length(tempM2))
@@ -44,7 +55,8 @@ for(i in seq_along(1:nrow(betweenSS))) {
     betweenSS[i, j] <- colSums(temp)
   }
 }
-betweenSS
+#betweenSS
+isSymmetric(betweenSS)
 
 # Weighted within sum of squares
 withinSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
@@ -54,10 +66,11 @@ for(i in seq_along(1:nrow(withinSS))) {
     temp <- matrix(-1, nrow = nrow(x0), ncol = 1)
     for(k in seq_along(1:nrow(temp))) {
 
-      currentGroupMean <- groupMeans[which(y0[k,] == unique(y0))]
+      currentGroupMean1 <- groupMeans[which(y0[k,] == unique(y0)), i]
+      currentGroupMean2 <- groupMeans[which(y0[k,] == unique(y0)), j]
 
-      tempM1 <- x0[k, ] - currentGroupMean
-      tempM2 <- x0[k, ] - currentGroupMean
+      tempM1 <- x0[k, i] - currentGroupMean1
+      tempM2 <- x0[k, j] - currentGroupMean2
 
       tempM1 <- matrix(tempM1, nrow = 1, ncol = length(tempM1))
       tempM2 <- matrix(tempM2, nrow = 1, ncol = length(tempM2))
@@ -69,44 +82,34 @@ for(i in seq_along(1:nrow(withinSS))) {
     withinSS[i, j] <- colSums(temp)
   }
 }
-withinSS
-
-
-
-# Weighted within sum of squares
-withinSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
-
-lookUp <- cbind(matrix(-1, nrow = nrow(x0), ncol = 1), x0)
-for(i in seq_along(1:nrow(lookUp))){
-  for(j in seq_along(1:nrow(unique(y0)))){
-    lookUp[i, 1] <- ifelse(y0[i,] == unique(y0)[j], groupMeans[j, ],lookUp[i, 1])
-  }
-}
-
-temp <- matrix(-1, nrow = nrow(x0), ncol =  ncol(groupMeans))
-for(i in seq_along(1:nrow(temp))){
-  for(j in seq_along(1:ncol(temp)))
-    temp[i, ] <- (lookUp[i, 2] - lookUp[i, 1]) * (lookUp[i, 2] - lookUp[i, 1])
-}
-temp
-
-withinSS <- colSums(temp)
-withinSS
-rm(temp)
+#withinSS
+isSymmetric(withinSS)
 
 # Total sum of squares
-lookUp <- cbind(matrix(rep(grandMean, nrow(x0)), nrow = nrow(x0), ncol = 1), x0)
 
-temp <- matrix(-1, nrow = nrow(x0), ncol =  ncol(groupMeans))
-for(i in seq_along(1:nrow(temp))){
-  for(j in seq_along(1:ncol(temp)))
-    temp[i, ] <- (lookUp[i, 2] - lookUp[i, 1]) * (lookUp[i, 2] - lookUp[i, 1])
+totalSS <- matrix(-1, nrow = ncol(grandMean), ncol = ncol(grandMean))
+
+for(i in seq_along(1:nrow(withinSS))) {
+  for(j in seq_along(1:ncol(withinSS))) {
+    temp <- matrix(-1, nrow = nrow(x0), ncol = 1)
+    for(k in seq_along(1:nrow(temp))) {
+      tempM1 <- x0[k, i] - grandMean[1,i]
+      tempM2 <- x0[k, j] - grandMean[1,j]
+
+      tempM1 <- matrix(tempM1, nrow = 1, ncol = length(tempM1))
+      tempM2 <- matrix(tempM2, nrow = 1, ncol = length(tempM2))
+      tempM2 <- t(tempM2)
+
+
+      temp[k, 1] <- tempM1 %*% tempM2
+    }
+    totalSS[i, j] <- colSums(temp)
+  }
 }
-temp
+#totalSS
+isSymmetric(totalSS)
+all(diag(totalSS)>0)
+all(diag(totalSS)^.5>9.8 & diag(totalSS)^.5<10.2)
 
-totalSS <- colSums(temp)
-totalSS
-rm(temp)
-
-withinSS + betweenSS == totalSS
+totalSS - (withinSS + betweenSS)
 
